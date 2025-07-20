0a5752ce4d266218da6f269bf9d80f2bef95040e
Nokk <ornelas.carlos@gmail.com>
2025-07-19 18:51:53 -0600

    fix(apiConfig): reorder export statements for consistency

--- src/apiConfig.ts
+++ src/apiConfig.ts
@@ -1,3 +1,3 @@
 export const BASE_URL = 'https://e6xg8ggbd2.execute-api.us-west-1.amazonaws.com';
-export const API_STAGE = import.meta.env.VITE_API_STAGE || 'PROD';
 export const AUTH_BASE_URL = 'https://iupws50sa8.execute-api.us-west-1.amazonaws.com';
+export const API_STAGE = import.meta.env.VITE_API_STAGE || 'PROD';
